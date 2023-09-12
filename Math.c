382892c1072c8188f4168beba941b477db0fc07a
Mike Njuki <mikenjuki@gmail.com>
2023-09-12 10:36:25 +0300

    Add project files.

---
+++ Math.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int MathProgram()
+{
+	int num = 9;
+	double A = sqrt(num);
+	double B = pow(2, num);
+	int C = round(3.145);
+	int D = ceil(3.4);
+	int E = floor(3.99);
+	double F = fabs(-200);
+	double G = log(3);
+	double H = sin(45);
+	double I = cos(45);
+	double J = tan(45);
+
+	printf("%lf\n", I);
+
+	return 0;
+}
---
+++ genesis.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int MathProgram();
+
+int main()
+{
+ /*   float item1 = 5.75;
+
+    printf("Item 1 price: %f ", item1);*/
+
+    printf("Program is below!\n");
+    MathProgram();
+
+    return 0;
+}
+
