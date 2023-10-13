2bcb293ddeff9345c60f607075b59e1439479cf2
Mike Njuki <mikenjuki@gmail.com>
2023-10-13 09:51:40 +0300

    largest of 10 using do while

--- Quiz.c
+++ Quiz.c
@@ -3,14 +3,31 @@
 int QuizPrograms()
 {
 	printf("\nFrom QuizPrograms!\n");
+	//initialize variables
+	// use a loop to check the count
+	// then compare the previous number to the currently entered number
+	// then stop the program when count is 10
+	//print out the largest at that point
 
-	//loop from 1 - 10
-	//print those numbers side by side on same line
-	// 3 space between each
+	//assumptions: user will input only integers and also no characters
 
-	for (int i = 1; i <= 10; i++)
+	int count = 1;
+	int number;
+	int largest = 0;
+
+	do
 	{
-		printf("%3d", i);
-	}
+		printf("\nEnter a number: ");
+		scanf_s("%d", &number);
+
+		if (number > largest)
+			largest = number;
+	
+		count++;
+
+	} while (count <= 10);
+
+	printf("Largest number of the 10 is: %d", largest);
+
 	return 0;
 }
