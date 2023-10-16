4a0566e097ca103f22de8d17537d4129f2704054
Mike Njuki <mikenjuki@gmail.com>
2023-10-16 12:36:33 +0300

    largest float

--- Quiz.c
+++ Quiz.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-#include <math.h>
-
-int findMultiple(int, int);
 
 int QuizPrograms()
 {
 	printf("\nFrom QuizPrograms!\n");
 
-	if (findMultiple(5, 10))
-	{
-		printf("\n 10 is a multiple of 5");
-	}
-	
+	int count = 1;
+	float userNum;
+	float largest = 0;
 
+	while (count <= 3)
+	{
+		printf("\nEnter float number: ");
+		scanf_s("%f", &userNum);
+
+		if (userNum > largest)
+			largest = userNum;
+
+		count++;
+	}
+
+	printf("\nLargest num is: %f", largest);
 
 	return 0;
 }
 
 
-int findMultiple(int num, int multiple)
-{
-	int check = multiple % num;
-
-	return (check == 0) ? 1 : 0;
-}
