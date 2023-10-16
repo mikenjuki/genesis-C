c08db5b4bc7496c16973536aa641c38a2e1fc545
Mike Njuki <mikenjuki@gmail.com>
2023-10-16 16:56:12 +0300

    multiplication program

--- Quiz.c
+++ Quiz.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int QuizPrograms()
 {
 	printf("\nFrom QuizPrograms!\n");
 
-	int count = 1;
-	float userNum;
-	float largest = 0;
+	int questions = 10, i, correctAnswers = 0;
 
-	while (count <= 3)
+	printf("\nLets practice multiplication on a set of 10");
+	for (i = 1; i <= questions; i++)
 	{
-		printf("\nEnter float number: ");
-		scanf_s("%f", &userNum);
+		int num1 = 1 + rand() % 9;
+		int num2 = 1 + rand() % 9;
+		int correctAns = num1 * num2;
 
-		if (userNum > largest)
-			largest = userNum;
+		printf("\nQuestion %d: What is %d times %d? ", i, num1, num2);
+		int userAns;
+		scanf_s("%d", &userAns);
 
-		count++;
+		if (userAns == -1) {
+			printf("Terminating the program.\n");
+			break;
+		}
+
+
+		if (userAns == correctAns)
+		{
+			printf("\nCorrect");
+			correctAnswers++;
+		}
+		else
+		{
+			printf("\nWrong! Try again.");
+			while (userAns != correctAns)
+			{
+				printf("\nQuestion %d: What is %d times %d? ", i, num1, num2);
+				scanf_s("%d", &userAns);
+			}
+		}
+
 	}
 
-	printf("\nLargest num is: %f", largest);
+	printf("You answered %d out of %d questions correctly.\n", correctAnswers, questions);
 
 	return 0;
 }
