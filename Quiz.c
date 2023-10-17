d279f2721a9f9b8fa082bc5da09663bb580a079d
Mike Njuki <mikenjuki@gmail.com>
2023-10-17 10:46:31 +0300

    playing with for loops and inputs

--- Quiz.c
+++ Quiz.c
@@ -6,23 +6,22 @@ int QuizPrograms()
 {
 	printf("\nFrom QuizPrograms!\n");
 
-	int n[SIZE] = { 19, 3, 15,7,11,9,13,5,17,1 };
-	int i, j;
+	
+	int i, sum = 0, x, y;
 
-	printf("\n%s%13s%17s\n", "Element", "Value", "Histogram");
-	for (i = 0; i <= SIZE - 1; i++)
+	printf("\nEnter number of integers you want to sum: ");
+	scanf_s("%d", &x);
+	for (i = 1; i <= x; i++)
 	{
-		printf("%7d%13d       ", i, n[i]);
-		for (j = 1; j <= n[i]; j++)
-		{
-			printf(" %c", '*');
-		}
-		printf("\n");
+		
+		printf("\nEnter a number: ");
+		scanf_s("%d", &y);
+		sum += y;
 	}
-
-
+	printf("The sum of the %d numbers you entered is: %d\n", x, sum);
 
 	return 0;
 }
 
-
+//for (j = 1; j <= i; j++)
+//printf(" %c", '@');
