92f7a66d4c3e7794210485efadea22d65c8b316a
Mike Njuki <mikenjuki@gmail.com>
2023-10-24 14:43:25 +0300

    playing with functions

---
+++ BubbleSort.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+void bubbleSort(int[], int);
+
+int Bubble()
+{
+	printf("\nFrom QuizPrograms!\n");
+
+	int sortArray[SIZE] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
+
+	printf("\nBefore sorting: %d %p", sortArray[0], &sortArray);
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("\n%3d", sortArray[i]);
+	}
+
+	bubbleSort(sortArray, SIZE);
+
+	printf("\nAfter sorting: %d %p\n", sortArray[0], &sortArray[0]);
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("\n%3d", sortArray[i]);
+	}
+
+	return 0;
+}
+
+void bubbleSort(int sortArray[], int size)
+{
+	int j, pass, hold;
+
+	for (j = 0; j <= size - 1; j++)
+	{
+		for (pass = 1; pass < size - 1 - j; pass++)
+		{
+			if (sortArray[pass] > sortArray[pass + 1])
+			{
+				hold = sortArray[pass];
+				sortArray[pass] = sortArray[pass + 1];
+				sortArray[pass + 1] = hold;
+			}
+		}
+
+	}
+
+}
+
+void sortColors(int* nums, int numsSize) {
+	int i, pass, hold;
+
+	for (i = 0; i <= numsSize - 1; i++)
+	{
+		for (pass = 0; pass < numsSize - 1 - i; pass++)
+		{
+			if (nums[pass] > nums[pass + 1])
+			{
+				hold = nums[pass];
+				nums[pass] = nums[pass + 1];
+				nums[pass + 1] = hold;
+			}
+		}
+	}
+}
+
+// Linear search algo
+
+/*
+#define SIZE 100
+
+int linearSearch(int[], int, int);
+
+int QuizPrograms()
+{
+	printf("\nFrom QuizPrograms!\n");
+
+	int a[SIZE], x, searchKey, element;
+
+	for (x = 0; x < SIZE - 1; x++)
+		a[x] = 2 * x;
+
+	// printf("\n%s%8s", "Index", "Value");
+	//for (x = 0; x < SIZE - 1; x++)
+	//	printf("\n%3d %8d", x, a[x]);
+
+
+	printf("\nEnter int search key:");
+	scanf_s("%d", &searchKey);
+
+	element = linearSearch(a, searchKey, SIZE);
+
+	if (element != -1)
+		printf("\nValue found in element: %d", element);
+	else
+		printf("\nValue not found");
+
+	return 0;
+}
+
+int linearSearch(const int array[], int key, int size)
+{
+	int n;
+
+	for (n = 0; n <= size - 1; ++n)
+		if (array[n] == key)
+			return n;
+
+	return -1;
+}
+
+*/
--- Quiz.c
+++ Quiz.c
@@ -1,66 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define SIZE 10
+#define SIZE 5
 
-void bubbleSort( int[], int);
+void calculateCharges(float [SIZE], float[SIZE], int);
 
 int QuizPrograms()
 {
 	printf("\nFrom QuizPrograms!\n");
 
-	int sortArray[SIZE] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37};
+	//enter number of hours parked for cars get print out
+	// enter as many customer hours and print out when you reach end
+	// enter numbers to array
+	// then loop over array and call calculateCharges and printout collective receipt
 
-	printf("\nBefore sorting: %d %p", sortArray[0], &sortArray);
-	for (int i = 0; i < SIZE ; i++)
+	int count = 0, i;
+	float clientHours;
+	float hoursArray[SIZE];
+	float amountArray[SIZE];
+
+	printf("\nCar Park Program");
+
+	do
 	{
-		printf("\n%3d", sortArray[i]);
+		printf("\nEnter car %d hours: ", count + 1);
+
+	
+		if (scanf_s("%f", &clientHours) != 1) {
+			printf("Invalid input. Please enter a valid number.\n");
+		
+		}
+		else {
+			hoursArray[count] = clientHours;
+			count++;
+		}
+
+	} while (count < SIZE);
+
+	calculateCharges(hoursArray, amountArray, SIZE);
+
+	printf("\n%s%15s%15s\n", "Car", "Hours", "Charge");
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("%3d%15.1f%14.2f\n", i + 1, hoursArray[i], amountArray[i]);
 	}
 
-	bubbleSort(sortArray, SIZE);
-
-	printf("\nAfter sorting: %d %p\n", sortArray[0], &sortArray[0]);
-	for (int i = 0; i < SIZE ; i++)
-	{
-		printf("\n%3d", sortArray[i]);
-	}
-
+	// printf("Here: %f", ceil(3.3));
+	
 	return 0;
    }
 
-void bubbleSort(int sortArray[], int size)
+void calculateCharges(float hours[SIZE], float amount[SIZE], int size)
 {
-	int j, pass, hold;
+	int count = 0, x;
 
-	for ( j = 0; j <= size - 1; j++)
+	for (int i = 0; i < size; i++)
 	{
-		for (pass = 1; pass < size - 1 - j; pass++)
+		if (hours[i] <= 3)
 		{
-			if (sortArray[pass] > sortArray[pass + 1])
-			{
-				hold = sortArray[pass];
-				sortArray[pass] = sortArray[pass + 1];
-				sortArray[pass + 1] = hold;
-			}
+			amount[i] = 2;
+		}
+		else if (hours[i] > 3 && hours[i] < 24)
+		{
+			printf("\nNumber is %f", hours[i]);
+			x = ceil(hours[i]);
+			amount[i] = 2 + (x - 3) * 0.5;
+			printf("\nAmount is %f", amount[i]);
+		}
+		else if (hours[i] == 24)
+		{
+			amount[i] = 10;
 		}
 
 	}
-	
-}
 
-void sortColors(int* nums, int numsSize) {
-	int i, pass, hold;
-
-	for (i = 0; i <= numsSize - 1; i++)
-	{
-		for (pass = 0; pass < numsSize - 1 - i; pass++)
-		{
-			if (nums[pass] > nums[pass + 1])
-			{
-				hold = nums[pass];
-				nums[pass] = nums[pass + 1];
-				nums[pass + 1] = hold;
-			}
-		}
-	}
 }
