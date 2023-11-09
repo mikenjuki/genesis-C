01c63c8c87bb9b76be7cb5008f6ca9512b6d1de6
Mike Njuki <mikenjuki@gmail.com>
2023-11-09 16:11:47 +0300

    card shuffle done

--- CardShuffle.c
+++ CardShuffle.c
@@ -5,6 +5,8 @@
 #define FSIZE 13
 #define SSIZE 4
 
+void shuffle(int [][FSIZE]);
+void deal(const int[][FSIZE], const char *[], const char *[]);
 
 int CardShuffle()
 {
@@ -15,5 +17,40 @@ int CardShuffle()
 
 	int deck[SSIZE][FSIZE] = { 0 };
 
+	//call this to randomize the selection later on
+	srand(time(0));
+
+	shuffle(deck);
+	deal(deck, face, suit);
+
 	return 0;
+}
+
+void shuffle(int cdeck [][FSIZE])
+{
+	//pick random spot in card array and insert number (1 - 52)
+	int card, row, column;
+	for (card = 1; card <= 52 ; card++)
+	{
+		do
+		{
+			row = rand() % SSIZE;
+			column = rand() % FSIZE;
+		} while (cdeck[row][column] != 0);
+
+		cdeck[row][column] = card;
+	}
+
+}
+
+void deal(const int cdeck [][FSIZE], const char *cface[], const char *csuit[])
+{
+	int card, row, column;
+	for (card = 1; card <= 52; card++)
+		for (row = 0; row <= SSIZE - 1; row++)
+			for (column = 0; column <= FSIZE - 1; column++)
+				if (cdeck[row][column] == card)
+					printf("%5s of %-8s%c", cface[column], csuit[row], card % 2 == 0 ? '\n' : '\t');
+				
+
 }
