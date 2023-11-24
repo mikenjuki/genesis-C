bd0d91ec821457564410c0dca270f38dd6a79d26
Mike Njuki <mikenjuki@gmail.com>
2023-11-24 10:45:53 +0300

    more character tasks

--- QuizProgram.c
+++ QuizProgram.c
@@ -17,6 +17,26 @@ int QuizProgram()
 	
 	printf("\v\v");
 	printf("%s\n%s%s\n", "isDigit check", isdigit(c) ? "c is a" : "c is not a", "digit");
+
+	printf("\v\v");
+	char* str = "-12345678";
+	char* endPtr;
+
+	x = strtol(str, &endPtr, 10);
+	printf("New long is: %ld \n", (long)x);
+
+	printf("\v\v");
+	int i = 0;
+	puts("Type something: ");
+	while ((y = getchar()) != '\n')
+		s1[i++] = y;
+
+	s1[i] = '\0';
+	puts("You entered: ");
+	puts(s1);
+
+
+
 	return 0;
 }
 
