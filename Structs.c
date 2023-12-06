ea6215cf1e6c2905334abdbd665be64cc0096c02
Mike Njuki <mikenjuki@gmail.com>
2023-12-06 12:32:49 +0300

    defining and accessing structs

--- Structs.c
+++ Structs.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+struct card
+{
+	char* face;
+	char* suit;
+};
+
 int Structs()
 {
-	printf("from structs");
+	printf("from structs\n");
+
+	struct card a;
+	struct card *aPtr;
+	
+	a.face = "Ace";
+	a.suit = "Spades";
+	aPtr = &a;
+
+	//access struct instance members in different ways
+	printf("\v%s%s%s\n", a.face, " of ", a.suit);
 
 	return 0;
 }
