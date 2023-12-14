1861467b7f924f6be67fa041417454843cbfe3e3
Mike Njuki <mikenjuki@gmail.com>
2023-12-14 11:44:40 +0300

    working with files

--- FileProcessing.c
+++ FileProcessing.c
@@ -7,7 +7,31 @@ int FileProcessing()
 	int account;
 	char name[30];
 	double balance;
+	FILE* cfPtr;
 
+	if (fopen_s(&cfPtr, "clientFile.txt", "w") != 0) {
+		printf("File could not be opened\n");
+		return -1;
+	}
+	else {
+		printf("Enter the account number, name, and balance.\n");
+		printf("Enter EOF to complete input.\n");
+		printf("? ");
+
+		scanf_s("%d %s %lf", &account, &name, (unsigned)sizeof(name), &balance);
+
+		while (scanf_s("%d %s %lf", &account, name, (unsigned)sizeof(name), &balance) == 3) {
+			if (strcmp(name, "exit") == 0) {
+				break; // Exit loop if user enters 'exit'
+			}
+
+			fprintf(cfPtr, "%d %s %.2f\n", account, name, balance);
+			printf("? ");
+			scanf_s("%d %s %lf", &account, (unsigned)sizeof(name), name, (unsigned)sizeof(balance), &balance);
+		}
+
+		fclose(cfPtr);
+	}
 
 
 	return 0;
